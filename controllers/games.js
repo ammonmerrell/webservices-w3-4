058fa63a2fdccdce52ceb07b985ddd23a0f1e750
ammonmerrell <ammonmerrell@outlook.com>
2026-05-19 08:09:26 -0600

    changed reference to database and new getAll games ref

--- controllers/games.js
+++ controllers/games.js
@@ -1,23 +1,14 @@
-const mongodb = require('mongodb');
+const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-// const getAll = async (req, res) => {
-//     const result = await mongodb.getDatabase('cluster0').db('games').collection('contacts').find();
-//     result.toArray().then((games) => {
-//         res.setHeader('Content-Type', 'application/json');
-//         res.status(200).json(games);
-//     });
-// };
-
-// old getAll from contacts 
 
 
 const getAll = async (req,res) => {
     //#swagger.tags=['contacts']
-    const result = await mongodb.getDatabase('cluster0').db('data0').collection('contacts').find();
-    result.toArray().then((contacts) => {
+    const result = await mongodb.getDatabase('cluster0').db('project2(w3-4)').collection('games').find();
+    result.toArray().then((games) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
+        res.status(200).json(games);
     });
 };
 
