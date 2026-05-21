36e93b1f6fdb6ecb135d108728f63387a7a7c6fe
ammonmerrell <ammonmerrell@outlook.com>
2026-05-20 20:34:56 -0600

    added validation due to instructor module

---
+++ utilities/validator.js
@@ -0,0 +1,69 @@
+// const util = require('.');
+
+const validator = require('./validatorjsPlugin.js')
+
+const saveGame = (req, res, next ) => {
+    const validationRule = {
+        name: 'required|string',
+        releaseDate: 'required|string',
+        platform: 'required|string',
+        genre: 'required|string',
+        description: 'required|string',
+        players: 'required|string',
+        type: 'required|string'
+    };
+    validator(req.body, validationRule, {}, (err, status) => {
+        if (!status) {
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err
+            });
+        } else {
+            next();
+        }
+    });
+};
+
+
+// const { body, validationResult } = require('express');
+// const inventoryModel = require("../controllers/games.js");
+// const validate = {};
+// name
+
+// validate.gamenameRules = () => {
+//     return [
+//         body('name')
+//         .notEmpty()
+//         .withMessage('Name is required'),
+
+//         body('releaseDate')
+//         .notEmpty()
+//         .isLength({ min: 10, max: 10})
+//         .withMessage('Release date is required, and must be 10 characters (mm/dd/yyyy)'),
+
+//         body('platform')
+//         .notEmpty()
+//         .withMessage('Platform is required, (Xbox, Playstation, Nintendo Switch)'),
+
+//         body('genre')
+//         .notEmpty()
+//         .withMessage('genre is required, (action-adventure, first-person shooter)'),
+
+//         body('description')
+//         .notEmpty()
+//         .withMessage('Description is required, can be a summary of game, or offical description'),
+
+//         body('players')
+//         .notEmpty()
+//         .withMessage('Number of players is required, (single-player, multi-player)'),
+
+//         body('type')
+//         .notEmpty()
+//         .withMessage('Type is required, (console, handheld)')
+//     ]
+// }
+
+module.exports = {
+    saveGame
+}
